ffafda33aed14d49cd01b45457422c4c9b8871ff
Almaas <jonasalmaas7@gmail.com>
2021-02-22 22:15:59 +0100

    Update addGame.js

--- scripts/gameProfiles/addGame.js
+++ scripts/gameProfiles/addGame.js
@@ -1,20 +1,18 @@
 const { ipcRenderer } = require('electron');
-const remote = require("electron").remote;
-const dialog = remote.dialog;
+const dialog = require("electron").remote.dialog;
 const fs = require('fs');
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-const configFolderPath = path.join(__dirname, '../config');
+const configFolderPath = path.join(__dirname, '../../config');
 const gamesPath = path.join(configFolderPath, 'games.json');
 
 const form = document.querySelector('form');
-const inputDir = document.getElementById('dir');
-const btnGetPath = document.getElementById('getPath');
-const btnCancel = document.getElementById('cancel');
+const inputDir = document.getElementById('path');
 
 form.addEventListener('submit', submitForm);
 
+const btnGetPath = document.getElementById('getPath');
 btnGetPath.addEventListener('click', (e) => {
     var gameDir = dialog.showOpenDialogSync({
         title: 'Select game directory',
@@ -26,8 +24,9 @@ btnGetPath.addEventListener('click', (e) => {
     }
 })
 
+const btnCancel = document.getElementById('cancel');
 btnCancel.addEventListener('click', (e) => {
-    ipcRenderer.send('addGame:close');
+    ipcRenderer.send('window:addGame:close');
 })
 
 function submitForm(e) {
@@ -45,15 +44,16 @@ function submitForm(e) {
     }
 
     const gameName = document.querySelector('#name').value;
-    const gameDir = document.querySelector('#dir').value;
+    const gameDir = document.querySelector('#path').value;
 
     jsonData[uuidv4()] = {
         name: gameName,
-        dir: gameDir
+        path: gameDir
     };
 
     var data = JSON.stringify(jsonData, null, 4);
     fs.writeFileSync(gamesPath, data);
 
-    ipcRenderer.send('addGame:close');
+    ipcRenderer.send('menu:games:update');
+    ipcRenderer.send('window:addGame:close');
 }
