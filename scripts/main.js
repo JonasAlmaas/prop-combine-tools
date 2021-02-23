d9aae21bc1b4baca010e3789c09a6091f7e397e1
Almaas <jonasalmaas7@gmail.com>
2021-02-23 22:36:27 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,43 @@
+const { app, BrowserWindow, Menu, nativeTheme } = require('electron');
+const url = require('url');
+const path = require('path');
+
 // process.env.NODE_ENV = 'production';
 
-require('./windowManager');
-require('./menuManager');
+const { mainMenuTemplate } = require('./templates');
+
+let windowMain;
+
+app.on('ready', function () {
+    windowMain = new BrowserWindow({
+        width: 1400,
+        height: 800,
+        minWidth: 1000,
+        minHeight: 500,
+        title: 'Prop Combine Tools',
+        show: false,
+        webPreferences: {
+            enableRemoteModule: true,
+            nodeIntegration: true
+        },
+    })
+
+    windowMain.once('ready-to-show', () => {
+        windowMain.show()
+    })
+
+    windowMain.setMenuBarVisibility(false);
+
+    windowMain.loadURL(url.format({
+        pathname: path.join(__dirname, '../pages/home.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    windowMain.on('closed', function () {
+        app.quit();
+    })
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+})
