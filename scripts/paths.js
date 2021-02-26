d540a0bf8d330f00c18392d89128b70c0d5c3536
Almaas <jonasalmaas7@gmail.com>
2021-02-26 02:28:45 +0100

    Create paths.js

---
+++ scripts/paths.js
@@ -0,0 +1,12 @@
+const electron = require('electron');
+const path = require('path');
+
+const appdata = (electron.app || electron.remote.app).getPath('userData');
+const projects = path.resolve(appdata, 'projects.json')
+const games = path.resolve(appdata, 'games.json')
+
+module.exports = {
+    appdata,
+    projects,
+    games
+}
