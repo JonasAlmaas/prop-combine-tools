ca2fab3c92191c738a1230016bf8b1539c1b1a9a
JonasAlmaas <44512227+JonasAlmaas@users.noreply.github.com>
2021-07-29 00:40:54 +0200

    Update Projects.js

--- src/scripts/pages/Projects.js
+++ src/scripts/pages/Projects.js
@@ -1,107 +1,83 @@
 const body = document.getElementsByTagName('body')[0];
 
-const { CreateStyleSheet, CreateDiv, CreateTitle, CreateLabel, CreateButton } = require('../utils/htmlGen.js');
+const { CreateStyleSheet, CreateDiv, CreateTitle, CreateLabel } = require('../utils/htmlGen.js');
+const { DataAndTimeToString } = require('../utils/date.js');
+const { SortingOption } = require('../utils/sorting.js');
 const paths = require('../utils/paths.js');
 
 const jsonData = {
     "516761bc-3fa1-4fba-a65e-f5a7ee5d3cd4": {
         "name": "Bavaria",
         "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
+        "dateModified": {
+            "year": 2004,
+            "month" : 11,
+            "day": 21,
+            "hour": 13,
+            "minute": 55,
+        },
+        "dateCreated": {
+            "year": 1995,
+            "month" : 10,
+            "day": 14,
+            "hour": 21,
+            "minute": 14,
+        }
     },
     "08b420f3-5452-4c19-86b3-09470ba42150": {
         "name": "Marine",
         "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
+        "dateModified": {
+            "year": 2007,
+            "month" : 11,
+            "day": 21,
+            "hour": 13,
+            "minute": 55,
+        },
+        "dateCreated": {
+            "year": 2001,
+            "month" : 10,
+            "day": 14,
+            "hour": 21,
+            "minute": 14,
+        }
     },
-    "51676bc-3fa1-4fba-a65e-f5a7ee5d3cd4": {
-        "name": "Bavaria",
+    "08b420f3-5452-4c19-86b3-090ba42150": {
+        "name": "Mostar",
         "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "08b42f3-5452-4c19-86b3-09470ba42150": {
-        "name": "Marine",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "516761bc-3fa1-4f-a65e-f5a7ee5d3cd4": {
-        "name": "Bavaria",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "08b420f3-5452-419-86b3-09470ba42150": {
-        "name": "Marine",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "516761bc-3fa1-4fba-a65ee5d3cd4": {
-        "name": "Bavaria",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "08b420f3-5452-4c19-86ba42150": {
-        "name": "Marine",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "51676bc-3fa1-4fba-a65e-f5a5d3cd4": {
-        "name": "Bavaria",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "08b42f3-5452-4c19-86b3-09470150": {
-        "name": "Marine",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "516761bca1-4f-a65e-f5a7ee5d3cd4": {
-        "name": "Bavaria",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
-    },
-    "08b423-5452-419-86b3-09470ba42150": {
-        "name": "Marine",
-        "game": "C:\\SteamLibrary\\steamapps\\common\\Counter-Strike Global Offensive\\csgo",
-        "dateLastModified": "14/10/2003",
-        "dateCreated": "23/06/2003",
-
+        "dateModified": {
+            "year": 2021,
+            "month" : 11,
+            "day": 21,
+            "hour": 13,
+            "minute": 55,
+        },
+        "dateCreated": {
+            "year": 2004,
+            "month" : 10,
+            "day": 14,
+            "hour": 21,
+            "minute": 14,
+        }
     }
 }
 
 var selectedProjectKey = null;
-var projectList = null;
+var sortingOption = null;
 
 var LoadPage = () => {
+    // Reset values
+    selectedProjectKey = null;
+    sortingOption = SortingOption.Name;
+
     {
-        var styleSheet = CreateStyleSheet('./styles/pages/Projects.css')
-        var styleSheetScrollbar = CreateStyleSheet('./styles/Scrollbar.css')
-        var styleSheetList = CreateStyleSheet('./styles/List.css')
+        var styleSheetScrollbar = CreateStyleSheet('./styles/scrollbar.css')
+        var styleSheetList = CreateStyleSheet('./styles/list.css')
+        var styleSheet = CreateStyleSheet('./styles/pages/projects.css')
     
-        body.append(styleSheet);
         body.append(styleSheetScrollbar);
         body.append(styleSheetList);
+        body.append(styleSheet);
     }
 
     var pageContainer = CreateDiv("page-container");
@@ -112,7 +88,25 @@ var LoadPage = () => {
         {
             var title = CreateTitle("Projects");
             projectListSorting = CreateDiv("listSorting-container");
-            projectList = CreateDiv("list-container");
+            {
+                var name = CreateDiv("sorting-option noselect");
+                name.innerHTML = "Name";
+                name.addEventListener('click', (e) => { sortingOption = SortingOption.Name; });
+
+                var dateModified = CreateDiv("sorting-option noselect");
+                dateModified.innerHTML = "Date modified";
+                dateModified.addEventListener('click', (e) => { sortingOption = SortingOption.DateModified; });
+
+                var dateCreated = CreateDiv("sorting-option noselect");
+                dateCreated.innerHTML = "Date Created";
+                dateCreated.addEventListener('click', (e) => { sortingOption = SortingOption.DateCreated; });
+
+                projectListSorting.appendChild(name);
+                projectListSorting.appendChild(dateModified);
+                projectListSorting.appendChild(dateCreated);
+            }
+            var projectList = CreateDiv("list-container");
+            projectList.id = "ProjectList"
             var projectSettings = CreateDiv("projectSettings-container");
             {
 
@@ -147,33 +141,45 @@ var LoadPage = () => {
 }
 
 function ReloadProjectList() {
+    var projectList = document.getElementById("ProjectList");
     projectList.innerHTML = '';
 
-    for (var key in jsonData) {
-        var listItemContainer = null;
-        {
-            if (key == selectedProjectKey) {
-                listItemContainer = CreateDiv("listItem-container listItem-container-selected noselect");
-            } else {
-                listItemContainer = CreateDiv("listItem-container noselect");
+    // var keyList = null;
+    // switch (sortingOption)
+    // {
+    //     case SortingOption.Name: { keyList = jsonData["sortedList"]["name"]; break; }
+    //     case SortingOption.DateModified: { keyList = jsonData["sortedList"]["dateModified"]; break; }
+    //     case SortingOption.DateCreated: { keyList = jsonData["sortedList"]["dateCreated"]; break; }
+    //     default { console.log(`Invalid sorting option! [${sortingOption}]`) }
+    // }
+
+    for (var i = 0; i < 1; i++) {
+        for (var key in jsonData) {
+            var listItemContainer = null;
+            {
+                if (key == selectedProjectKey) {
+                    listItemContainer = CreateDiv("listItem-container listItem-container-selected noselect");
+                } else {
+                    listItemContainer = CreateDiv("listItem-container noselect");
+                }
+                listItemContainer.id = key;
+                
+                listItemContainer.addEventListener('click', (e) => {
+                    selectedProjectKey = e.target.id;
+                    ReloadProjectList();
+                });
+    
+                var listItemLabel = CreateLabel("", jsonData[key]['name']);
+                var dateLastModifiedLabel = CreateLabel("listItem-date", DataAndTimeToString(jsonData[key]['dateModified']));
+                var dateCreatedLabel = CreateLabel("listItem-date", DataAndTimeToString(jsonData[key]['dateCreated']));
+
+                listItemContainer.appendChild(listItemLabel);
+                listItemContainer.appendChild(dateLastModifiedLabel);
+                listItemContainer.appendChild(dateCreatedLabel);
             }
-            listItemContainer.id = key;
-            
-            listItemContainer.addEventListener('click', (e) => {
-                selectedProjectKey = e.target.id;
-                ReloadProjectList();
-            });
-
-            var listItemLabel = CreateLabel("", jsonData[key]['name']);
-            var dateLastModifiedLabel = CreateLabel("listItem-date", jsonData[key]['dateLastModified']);
-            var dateCreatedLabel = CreateLabel("listItem-date", jsonData[key]['dateCreated']);
-
-            listItemContainer.appendChild(listItemLabel);
-            listItemContainer.appendChild(dateLastModifiedLabel);
-            listItemContainer.appendChild(dateCreatedLabel);
+    
+            projectList.appendChild(listItemContainer);
         }
-
-        projectList.appendChild(listItemContainer);
     }
 }
 
