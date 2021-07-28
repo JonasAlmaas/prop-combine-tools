ebe7ff831128358a7ed921572038d64b25bbca16
Almaas <jonasalmaas7@gmail.com>
2021-07-28 09:03:31 +0200

    Made htmlGen work with Node.js

--- src/scripts/utils/htmlGen.js
+++ src/scripts/utils/htmlGen.js
@@ -1,33 +1,41 @@
-export function CreateStyleSheet(path) {
+var CreateStyleSheet = (path) => {
     var styleSheet = document.createElement('link');
     styleSheet.rel = 'stylesheet';
     styleSheet.href = path;
     return styleSheet;
 }
 
-export function CreateDiv(classes) {
+var CreateDiv = (classes) => {
     var div = document.createElement('div');
     div.className = classes;
     return div;
 }
 
-export function CreateTitle(title) {
+var CreateTitle = (title) => {
     var div = CreateDiv("title-container");
     var title = CreateLabel("title noselect", title)
     div.appendChild(title);
     return div;
 }
 
-export function CreateLabel(classes, text) {
+var CreateLabel = (classes, text) => {
     var div = document.createElement('div');
     div.className = classes;
     div.innerHTML = text;
     return div;
 }
 
-export function CreateButton(classes, text) {
+var CreateButton = (classes, text) => {
     var btn = document.createElement("button");
     btn.innerHTML = text;
     btn.className = classes;
     return btn;
+}
+
+module.exports = {
+    CreateStyleSheet,
+    CreateDiv,
+    CreateTitle,
+    CreateLabel,
+    CreateButton,
 }
